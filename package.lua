78cc0ffc329f3d590c7b978bb76c70f8849172ed
console.log(cat == cute) <46598063+qwreey75@users.noreply.github.com>
2022-01-22 20:52:41 +0900

    Create package.lua

---
+++ package.lua
@@ -0,0 +1,11 @@
+return {
+	name = 'qwreey75/promise',
+	version = '1.0.1',
+	homepage = 'https://github.com/qwreey75/promise.lua',
+	dependencies = {
+	},
+	tags = {'coroutine','promise','async'},
+	license = 'MIT',
+	author = 'qwreey75',
+	files = {'**.lua'},
+}
