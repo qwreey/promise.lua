a22aea7f8876cd569433e4ebd1186ecc603115ed
세젤귀 고양이들 <46598063+qwreey75@users.noreply.github.com>
2022-03-13 21:00:42 +0900

    update unpack

--- init.lua
+++ init.lua
@@ -16,15 +16,16 @@ local function unpack(t,n,p)
 	if not n then n = t.n or #t end
 	if (not n) or n == 0 then
 		return;
-	elseif n == p then
+	elseif n >= p then
 		return t[n];
 	elseif not p then
-		p = 1;
 		if n == 1 then return t[1]; end
+		p = 1;
 		return t[1],unpack(t,n,p+1);
 	end
 	return t[p],unpack(t,n,p+1);
 end
+
 --#endregion --* Const *--
 --#region --* Log/Debug *--
 ---@diagnostic disable
