cb6c9a00161e6d54f569c0aed520c142b0ddb2e8
세젤귀 고양이들 <46598063+qwreey75@users.noreply.github.com>
2022-03-13 21:51:19 +0900

    Removed unpack (cuz it making bugs)

--- init.lua
+++ init.lua
@@ -12,21 +12,22 @@ local yield = coroutine.yield;
 local running = coroutine.running;
 local resume = coroutine.resume;
 local pack = table.pack;
-local oldUnpack = unpack;
-local function unpack(t,n,p)
-	if not n then n = t.n end
-	if not n then return oldUnpack(t) end
-	if (not n) or n == 0 then
-		return;
-	elseif not p then
-		if n == 1 then return t[1]; end
-		p = 1;
-		return t[1],unpack(t,n,p+1);
-	elseif n >= p then
-		return t[n];
-	end
-	return t[p],unpack(t,n,p+1);
-end
+local unpack = table.unpack;
+-- local oldUnpack = unpack;
+-- local function unpack(t,n,p)
+-- 	if not n then n = t.n end
+-- 	if not n then return oldUnpack(t) end
+-- 	if (not n) or n == 0 then
+-- 		return;
+-- 	elseif not p then
+-- 		if n == 1 then return t[1]; end
+-- 		p = 1;
+-- 		return t[1],unpack(t,n,p+1);
+-- 	elseif n >= p then
+-- 		return t[n];
+-- 	end
+-- 	return t[p],unpack(t,n,p+1);
+-- end
 
 --#endregion --* Const *--
 --#region --* Log/Debug *--
