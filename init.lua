3232c3a2826f9f48c0a2e0de841335b9cc392d5e
세젤귀 고양이들 <46598063+qwreey75@users.noreply.github.com>
2022-03-13 21:19:23 +0900

    Update promise module

--- init.lua
+++ init.lua
@@ -16,12 +16,12 @@ local function unpack(t,n,p)
 	if not n then n = t.n or #t end
 	if (not n) or n == 0 then
 		return;
-	elseif n >= p then
-		return t[n];
 	elseif not p then
 		if n == 1 then return t[1]; end
 		p = 1;
 		return t[1],unpack(t,n,p+1);
+	elseif n >= p then
+		return t[n];
 	end
 	return t[p],unpack(t,n,p+1);
 end
